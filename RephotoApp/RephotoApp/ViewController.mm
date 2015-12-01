f972541ca5d8f48db4307bf5ea0983205e339fb1
Nayeon Kim <nayeonk@mit.edu>
2015-12-01 16:45:48 -0500

    buggy canny edge detection

--- RephotoApp/RephotoApp/ViewController.mm
+++ RephotoApp/RephotoApp/ViewController.mm
@@ -67,7 +67,41 @@
     [self dismissModalViewControllerAnimated:YES];
     
     cv::Mat cvImg = [self UIImageToMat:image];
-    self.refImage = cvImg;
+    
+    // Canny Edge Detector with picked image
+    
+    Mat cvImg_gray;
+    Mat cvMatrix, detected_edges;
+    
+//    int edgeThresh = 1;
+//    int lowThreshold;
+//    int const max_lowThreshold = 100;
+    int ratio = 3;
+    int kernel_size = 3;
+    
+    /// Create a matrix of the same type and size as src (for dst)
+    cvMatrix.create( cvImg.size(), cvImg.type() );
+    
+    /// Convert the image to grayscale
+    cvtColor( cvImg, cvImg_gray, CV_BGR2GRAY );
+    
+    int CannyThreshold = 20.0;
+    
+    /// Show the image
+//    CannyThreshold(0, 0);
+    
+    /// Reduce noise with a kernel 3x3
+//    blur( cvImg_gray, cvImg_gray, cv::Size(3,3) );
+        GaussianBlur(cvImg_gray, cvImg_gray, cvSize(5, 5),1.2,1.2);//remove small details
+    /// Canny detector
+    Canny( cvImg_gray, detected_edges, CannyThreshold, CannyThreshold*ratio, kernel_size );
+    
+    /// Using Canny's output as a mask, we display our result
+    cvMatrix = Scalar::all(0.0);
+
+    cvImg.copyTo( cvMatrix, detected_edges);
+    
+    self.refImage = cvMatrix;
     self.imageLoaded = true;
 }
 
@@ -75,8 +109,17 @@
 - (cv::Mat) UIImageToMat: (UIImage *)loadedImage;
 {
     CGColorSpaceRef colorSpace = CGImageGetColorSpace(loadedImage.CGImage);
-    CGFloat cols = loadedImage.size.width;
-    CGFloat rows = loadedImage.size.height;
+    
+    UIImageOrientation orientation = loadedImage.imageOrientation;
+    CGFloat cols, rows;
+    if  (orientation == UIImageOrientationLeft
+         || orientation == UIImageOrientationRight) {
+        cols = loadedImage.size.height;
+        rows = loadedImage.size.width;
+    } else {
+        cols = loadedImage.size.width;
+        rows = loadedImage.size.height;
+    }
     
     cv::Mat cvMat(rows, cols, CV_8UC4);                                 // 8 bits per component, 4 channels
     CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to backing data
@@ -98,8 +141,8 @@
 #ifdef __cplusplus
 - (void)processImage:(Mat&)image;
 {
-    double alpha = 0.7;
-    double beta = 0.3;
+    double alpha = 0.3;
+    double beta = 0.7;
     
     Mat refIm;
     NSLog(@"processImage began running");
